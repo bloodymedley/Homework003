6f77b21a43916a8deeff32873bd5b5d0a8a3a842
bloodymedley <de4ster@gmail.com>
2023-08-15 16:31:28 +0300

    задание3

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+if ( x % 2 == 0){
+    Console.WriteLine("Число четное");
+}
+else{
+    Console.WriteLine("Число нечетное");
+}
